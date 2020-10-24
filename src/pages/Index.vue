63115e0e152516baa2817500de7b518190b9d42f
HugoMatilla <hugomatilla@gmail.com>
2020-10-24 13:55:11 +0200

    Add popup to show in Safari does not look good

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -43,6 +43,23 @@ export default {
     SectionBase,
     SectionFullWidth,
   },
+  methods: {
+      showSafariPopUp: function () {
+        var isSafari = navigator.vendor.toLowerCase().indexOf('apple') > -1
+        if(isSafari && localStorage.getItem("dismissSafariToast")==null){ 
+        this.$buefy.snackbar.open({
+            message: `This site looks better in Chrome or FireFox `,
+            type: 'is-warning',
+            position: 'is-top',
+            duration: 5000,
+            actionText: "Dismiss",
+            onAction: () => { localStorage.setItem("dismissSafariToast", true); }})
+        }
+      }
+  },
+  mounted() {
+    setTimeout(this.showSafariPopUp, 1500)
+  }
 };
 </script>
 
